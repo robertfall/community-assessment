9feb57ffc6f130f92660eed1f5ff7311891198a8
Robert Herbst <robert.herbst@allangray.co.za>
2016-07-12 20:24:11 +0200

    Fix CheckBoxes control

--- app/households/components/inputs/CheckBoxes.jsx
+++ app/households/components/inputs/CheckBoxes.jsx
@@ -1,69 +1,80 @@
 import React, { Component, PropTypes } from 'react';
 import startCase from 'lodash/startCase';
 import chunk from 'lodash/chunk';
+import without from 'lodash/without';
+import includes from 'lodash/includes';
 import { Field } from 'redux-form';
 
-export default class CheckBoxes extends Component {
-  static propTypes = {
+const parse = (option, value, onChange) =>
+  ({ target }) => {
+    const safeValue = value || [];
+
+    if (includes(safeValue, option)) {
+      if (!target.checked) { onChange(without(safeValue, option)); }
+    } else {
+      if (target.checked) { onChange(safeValue.concat(option)); }
+    }
+  };
+
+
+const realName = (name) => name.split('.')[-1];
+
+const children = ({ options, name, includeOther, value, onChange }) => {
+  const optionsWithOther = includeOther
+                         ? options.concat('Other')
+                         : options;
+
+  return chunk(optionsWithOther, 3).map((row, index) => (
+    <div key={index}>
+      {
+        row.map((opt) => (
+          <div key={opt} className="col-sm-4">
+            <label className="checkbox-inline control-label">
+              <input
+                type="checkbox"
+                name={name}
+                onChange={parse(opt, value, onChange)}
+                checked={includes(value, opt)}
+              />
+              {startCase(opt)}
+            </label>
+          </div>
+        ))
+      }
+    </div>
+  ));
+};
+
+const CheckBoxes = ({ input }) => {
+  const { label, name } = input;
+  return (
+    <div>
+      <label
+        htmlFor={name}
+        className="col-md-4 control-label"
+      >
+        {label || startCase(realName(name))}
+      </label>
+      <div className="col-md-6">
+        {children(input)}
+      </div>
+    </div>
+  );
+};
+
+CheckBoxes.propTypes = {
+  input: PropTypes.shape({
+    value: PropTypes.any.isRequired,
     options: PropTypes.array.isRequired,
     name: PropTypes.string,
     label: PropTypes.string,
-    includeOther: PropTypes.bool
-  };
+    includeOther: PropTypes.bool,
+  }).isRequired,
+};
 
-  labelText() {
-    return this.props.label
-         ? this.props.label
-         : startCase(this.realName());
-  }
-
-  realName() {
-    if (this.props.name) return this.props.name;
-
-    const pathParts = this.props.name.split('.');
-    return pathParts[pathParts.length - 1];
-  }
-
-
-  children() {
-    const { options, includeOther } = this.props;
-    const optionsWithOther = includeOther
-                           ? options.concat('Other')
-                           : options;
-
-    return chunk(optionsWithOther, 3).map((row, index) => (
-      <div key={index}>
-        {
-          row.map((opt) => (
-            <div key={opt} className="col-sm-4">
-              <label className="checkbox-inline control-label">
-                <Field
-                  component="input"
-                  type="checkbox"
-                  name={this.props.name}
-                />
-                {startCase(opt)}
-              </label>
-            </div>
-          ))
-        }
-      </div>
-    ));
-  }
-
-  render() {
-    return (
-      <div>
-        <label
-          htmlFor={this.realName()}
-          className="col-md-4 control-label"
-        >
-          {this.labelText()}
-        </label>
-        <div className="col-md-6">
-          {this.children()}
-        </div>
-      </div>
-    );
-  }
-}
+export default (props) => (
+  <Field
+    component={CheckBoxes}
+    {...props}
+  />
+);
